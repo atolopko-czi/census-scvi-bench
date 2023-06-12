e1bcfe6a44d1804e25583cca151b0bd4e82f9e54
Andrew Tolopko <atolopko-czi@users.noreply.github.com>
2023-06-12 14:54:57 -0400

    Update tests.sh

--- tests.sh
+++ tests.sh
@@ -1,3 +1,4 @@
+# params: obs_filter, gpu count, pytorch batch size, epochs
 /usr/bin/time -v python census_scvi.py "tissue_general == 'lung' and is_primary_data == True" 1 128 1  > logs/lung_1gpu_128batch_1epoch.txt 2>&1
 /usr/bin/time -v python census_scvi.py "tissue_general == 'lung' and is_primary_data == True" 2 128 1  > logs/lung_2gpu_128batch_1epoch.txt 2>&1
 /usr/bin/time -v python census_scvi.py "tissue_general == 'lung' and is_primary_data == True" 4 128 1  > logs/lung_4gpu_128batch_1epoch.txt 2>&1
