3535177d7cbc19b3e9de5ccbe45edf164b5d3a0c
Ubuntu <ubuntu@ip-172-31-6-212.us-west-2.compute.internal>
2023-06-14 13:11:58 +0000

    update selected timings

--- selected_timings.sh
+++ selected_timings.sh
@@ -1,6 +1,11 @@
-egrep 'wall clock' logs/heart*txt > logs/selected_timings.txt
-egrep 'run_training_epoch' logs/heart*txt >> logs/selected_timings.txt
-egrep 'run_training_batch' logs/heart*txt >> logs/selected_timings.txt
-egrep 'train_dataloader_next' logs/heart*txt >> logs/selected_timings.txt
-egrep 'DDPStrategy.backward' logs/heart*txt >> logs/selected_timings.txt
-egrep 'DDPStrategy.training_step' logs/heart*txt >> logs/selected_timings.txt
+egrep 'wall clock' logs/heart_?gpu_128batch_1epoch*txt > logs/selected_timings.txt
+echo >> logs/selected_timings.txt
+egrep 'Retrieved batch' logs/heart_?gpu_128batch_1epoch*txt >> logs/selected_timings.txt
+echo >> logs/selected_timings.txt
+egrep 'Total' logs/heart_?gpu_128batch_1epoch*txt >> logs/selected_timings.txt
+echo >> logs/selected_timings.txt
+egrep 'run_training_epoch' logs/heart_?gpu_128batch_1epoch*txt >> logs/selected_timings.txt
+egrep 'run_training_batch' logs/heart_?gpu_128batch_1epoch*txt >> logs/selected_timings.txt
+egrep 'train_dataloader_next' logs/heart_?gpu_128batch_1epoch*txt >> logs/selected_timings.txt
+egrep 'DDPStrategy.backward' logs/heart_?gpu_128batch_1epoch*txt >> logs/selected_timings.txt
+egrep 'DDPStrategy.training_step' logs/heart_?gpu_128batch_1epoch*txt >> logs/selected_timings.txt
