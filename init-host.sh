23ebf5c05180c2d70782c549f935e847c89b1619
Ubuntu <ubuntu@ip-172-31-6-212.us-west-2.compute.internal>
2023-08-03 18:15:57 +0000

    Update init-host.sh

--- init-host.sh
+++ init-host.sh
@@ -1,5 +1,7 @@
 sudo apt update
-sudo apt install python3.10-venv emacs
+sudo apt install -u software-properties-common
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt install python3.10-venv
 python3.10 -m venv venv
 . ~/venv/bin/activate
 git clone https://github.com/atolopko-czi/census-scvi-bench.git
