a178861f7ff0b30f5a4370b0921c0c0eb47ab871
Ubuntu <ubuntu@ip-172-31-6-212.us-west-2.compute.internal>
2023-09-07 17:28:12 +0000

    init-host.sh updates

--- init-host.sh
+++ init-host.sh
@@ -7,4 +7,9 @@ python3.10 -m venv venv
 git clone https://github.com/atolopko-czi/census-scvi-bench.git
 cd census-scvi-bench
 pip install -r requirements.txt
+sudo mount /dev/nvme1n1 /mnt/  # mount census volume
+export PYTHONPATH=/home/ubuntu/cellxgene-census/api/python/cellxgene_census/src/:$PYTHONPATH
+
 python census_scvi.py
+
+
